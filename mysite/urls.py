b27596c064407b86cdc5eaa871c37e0edecfb1e7
Pedro <pedrohfonseca01@gmail.com>
2023-10-26 23:47:59 -0300

    Iniciando projeto

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from main_site import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', views.TemplatePrincipal.as_view(), name='index')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
